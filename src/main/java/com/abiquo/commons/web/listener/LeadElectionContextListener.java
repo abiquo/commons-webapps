f8500079882c7e1515f85714e83679ebea64e289
Albert Puig <apuig@abiquo.com>
2017-06-16 16:25:52 +0200

    use apache curator ABICLOUDPREMIUM-9687 #comment

--- src/main/java/com/abiquo/commons/web/listener/LeadElectionContextListener.java
+++ src/main/java/com/abiquo/commons/web/listener/LeadElectionContextListener.java
@@ -6,41 +6,35 @@
  */
 package com.abiquo.commons.web.listener;
 
-import static com.netflix.curator.framework.CuratorFrameworkFactory.newClient;
 import static java.lang.Integer.valueOf;
 import static java.lang.System.getProperty;
 import static java.lang.Thread.currentThread;
+import static org.apache.curator.framework.CuratorFrameworkFactory.newClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListener;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.retry.RetryNTimes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.abiquo.commons.web.ClusterConstants;
-import com.netflix.curator.RetryLoop;
-import com.netflix.curator.framework.CuratorFramework;
-import com.netflix.curator.framework.CuratorFrameworkFactory;
-import com.netflix.curator.framework.api.CuratorWatcher;
-import com.netflix.curator.framework.recipes.leader.LeaderSelector;
-import com.netflix.curator.framework.recipes.leader.LeaderSelectorListener;
-import com.netflix.curator.framework.state.ConnectionState;
-import com.netflix.curator.retry.RetryNTimes;
 
 /**
  * Check for node distribution to directly use the {@link AMQPConsumersService} or delegate it to
  * the cluster leader notification.
  */
-public abstract class LeadElectionContextListener implements ServletContextListener,
-    LeaderSelectorListener, CuratorWatcher
+public abstract class LeadElectionContextListener extends LeaderSelectorListenerAdapter implements
+    ServletContextListener
+
 {
     /** Tune {@link CuratorFrameworkFactory}. Connection timeout */
     private static final int ZK_CONNECTION_TIMEOUT_MS = valueOf(getProperty("abiquo.api.zk."
@@ -122,29 +116,24 @@ public abstract class LeadElectionContextListener implements ServletContextListe
     /**
      * Get the path for the node in Zookeeper.
      */
-    public abstract String getZookeeperNodePath();
+    private String zookeeperNodePath;
 
     @Override
     public void contextDestroyed(final ServletContextEvent sce)
     {
         if (isDistributed())
         {
-            if (leaderSelector.hasLeadership())
-            {
-                onShutdown(sce);
-            }
+            stopZookeeper();
+        }
 
-            leaderSelector.close();
-        }
-        else
-        {
-            onShutdown(sce);
-        }
+        onShutdown(sce);
     }
 
     @Override
     public void contextInitialized(final ServletContextEvent sce)
     {
+        zookeeperNodePath = sce.getServletContext().getContextPath() + "/leader-election";
+
         initializeContext(sce);
 
         if (isDistributed())
@@ -153,12 +142,13 @@ public abstract class LeadElectionContextListener implements ServletContextListe
             {
                 startZookeeper();
             }
-            // if zk-server not started then the node can't start properly
             catch (Exception e)
             {
-                LOGGER.error("Fatal, zookeeper configuration enabled "
-                    + "but not connection to zk server at {}", ZK_SERVER, e);
-                throw new RuntimeException(e);
+                String error =
+                    "Cannot start, zookeeper configuration enabled but not connection to zk server at "
+                        + ZK_SERVER;
+                LOGGER.error(error, e);
+                throw new RuntimeException(error, e);
             }
         }
         else
@@ -167,153 +157,52 @@ public abstract class LeadElectionContextListener implements ServletContextListe
         }
     }
 
-    @Override
-    public void process(final WatchedEvent event) throws Exception
-    {
-        if (event.getType() == EventType.NodeDeleted)
-        {
-            LOGGER.info("Processing deletion of Zookeeper path " + event.getPath());
-
-            if (leaderSelector.hasLeadership())
-            {
-                onLeadershipSuspended();
-            }
-
-            leaderSelector.close();
-
-            // reinitialize the leader selector
-            leaderSelector = new LeaderSelector(curatorClient, getZookeeperNodePath(), this);
-            leaderSelector.setId(getHostName());
-            leaderSelector.start();
-
-            LOGGER.info("Starting Leader selector instance");
-        }
-    }
-
     /**
-     * If the SUSPENDED state is reported, the instance must assume that it might no longer be the
-     * leader until it receives a RECONNECTED state. If the LOST state is reported, the instance is
-     * no longer the leader and its takeLeadership method should exit.
-     */
-    @Override
-    public void stateChanged(final CuratorFramework client, final ConnectionState newState)
-    {
-        LOGGER.info("{} to {}", newState.name(), getZookeeperNodePath());
-
-        switch (newState)
-        {
-            case SUSPENDED:
-                if (leaderSelector.hasLeadership())
-                {
-                    onLeadershipSuspended();
-                }
-                break;
-            case RECONNECTED:
-                // theory leaderSelector.requeue();
-                // https://github.com/Netflix/curator/issues/24
-                // The method #process will create a new leaderSelector instance. Once is
-                // reconnected, watch again
-                LOGGER.info("Processing Zookeeper reconnect");
-                // reinitialize the leader selector
-                leaderSelector = new LeaderSelector(curatorClient, getZookeeperNodePath(), this);
-                leaderSelector.setId(getHostName());
-                leaderSelector.start();
-
-                watchItself();
-                break;
-
-            case CONNECTED:
-                LOGGER.info("Processing Zookeeper connect");
-                watchItself();
-                break;
-
-            case LOST:
-                // already disconnected by SUSPENDED state
-                break;
-        }
-    }
-
-    /**
-     * Only leader has the AMQP consumers started.
-     * <p>
      * /!\ NOTE : This method should only return when leadership is being relinquished.
      */
     @Override
     public void takeLeadership(final CuratorFramework client) throws Exception
     {
-        onLeadershipTaken();
+        Exception failedToTake = null;
+        try
+        {
+            LOGGER.info("Taking leadership on {} ...", zookeeperNodePath);
+            onLeadershipTaken();
+        }
+        catch (Exception e)
+        {
+            failedToTake = e;
+            LOGGER.error("Failed to take leadership on " + zookeeperNodePath, e);
+        }
+
+        if (failedToTake == null)
+        {
+            try
+            {
+                LOGGER.info("Leader on {}", zookeeperNodePath);
+                currentThread().join();
+            }
+            catch (InterruptedException e)
+            {
+                LOGGER.info("leadership interrupted");
+            }
+
+            LOGGER.info("Current node no longer the {} leader", zookeeperNodePath);
+        }
 
         try
         {
-            currentThread().join(); // do not return
+            onLeadershipSuspended();
         }
-        catch (InterruptedException e)
+        catch (Exception e)
         {
-            // expected during ''contextDestroy''
+            LOGGER.warn("Fail to cleanup onLeadershipSuspended on " + zookeeperNodePath, e);
         }
 
-        // if interrupted by a ''stateChanged'' the handler will check if require
-        // ''shutdownRegisteredConsumers''
-        LOGGER.info("Current node no longer the {} leader", getZookeeperNodePath());
-    }
-
-    private void watchItself()
-    {
-        // Due the asynchronous nature of zookeeper, the getChildren can raise an
-        // exception because the last znode could not be yet created when we are
-        // looking for it. So the best solution is use the RetryLoop as Curator
-        // recommends.
-        // (Retry Policy is configured at initialization time)
-        RetryLoop rl = curatorClient.getZookeeperClient().newRetryLoop();
-        List<String> child;
-        int i = 0;
-
-        while (rl.shouldContinue())
+        if (failedToTake != null)
         {
-            LOGGER.info("Trying to put a watcher in the last node created. (" + i + " attempts )");
-            try
-            {
-                Thread.sleep(3000);
-
-                child = curatorClient.getChildren().forPath(getZookeeperNodePath());
-
-                Collections.sort(child, new Comparator<String>()
-                {
-                    // names of the znodes are like 'generateduuid'-lock-seq
-                    // we need to compare only the seq
-                    @Override
-                    public int compare(final String o1, final String o2)
-                    {
-
-                        Integer seq1 = Integer.valueOf(o1.substring(o1.indexOf("lock-") + 5));
-                        Integer seq2 = Integer.valueOf(o2.substring(o2.indexOf("lock-") + 5));
-
-                        // we want ordered descending
-                        return seq2.compareTo(seq1);
-
-                    }
-                });
-
-                String path = getZookeeperNodePath() + "/" + child.get(0);
-                LOGGER.info("Starting watcher for path " + path);
-                curatorClient.checkExists().usingWatcher(this).forPath(path);
-
-                rl.markComplete();
-            }
-            catch (Exception e)
-            {
-                try
-                {
-                    LOGGER.warn("Node could not execute the 'watchItself' in Zookeeper");
-                    rl.takeException(e);
-                }
-                catch (Exception e1)
-                {
-                    LOGGER.error("Could not Watch 'itself' node.");
-                }
-            }
+            throw failedToTake;
         }
-
     }
 
     /** Connects to ZK-Server and adds as participant to {@link LeaderSelector} cluster. */
@@ -325,9 +214,38 @@ public abstract class LeadElectionContextListener implements ServletContextListe
 
         LOGGER.info("Connected to {}", ZK_SERVER);
 
-        leaderSelector = new LeaderSelector(curatorClient, getZookeeperNodePath(), this);
+        leaderSelector = new LeaderSelector(curatorClient, zookeeperNodePath, this);
+        leaderSelector.autoRequeue();
         leaderSelector.setId(getHostName());
         leaderSelector.start();
+
+        LOGGER.info("Participating in leader selector at {}", zookeeperNodePath);
+    }
+
+    protected void stopZookeeper()
+    {
+        LOGGER.debug("closing LeaderSelector ...");
+        try
+        {
+
+            leaderSelector.close();
+        }
+        catch (Exception e)
+        {
+            LOGGER.warn("Cannot close leaderSelector", e);
+        }
+        try
+        {
+
+            curatorClient.close();
+        }
+        catch (Exception e)
+        {
+            LOGGER.warn("Cannot close curatorClient", e);
+        }
+
+        leaderSelector = null;
+        curatorClient = null;
     }
 
     /**
