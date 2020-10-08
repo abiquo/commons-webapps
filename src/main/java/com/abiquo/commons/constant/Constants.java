319bd1aac393d70ca991aa35494c179ca840f103
Hector Herranz <hector.herranz@abiquo.com>
2020-10-08 12:22:45 +0200

    Added new CLOUDNODE_UUID_HEADER constant ABICLOUDPREMIUM-13884

---
+++ src/main/java/com/abiquo/commons/constant/Constants.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2008 - Abiquo Holdings S.L. All rights reserved.
+ *
+ * Please see /opt/abiquo/tomcat/webapps/legal/ on Abiquo server
+ * or contact contact@abiquo.com for licensing information.
+ */
+package com.abiquo.commons.constant;
+
+public class Constants
+{
+    /**
+     * Header that allows API to get the Cloudnode UUID from VSM (amqp header)
+     */
+    public static final String CLOUDNODE_UUID_HEADER = "X-Abiquo-CloudNodeUuid";
+
+}
